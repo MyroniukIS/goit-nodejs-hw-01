50c262ad0d256407dada7a8ff1e1c98deb6519fd
Myroniuk Ilona <knopkakyrs@gmail.com>
2022-01-22 03:52:43 +0200

    install commander and update my code to commander

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const yargs = require("yargs");
-const { hideBin } = require("yargs/helpers");
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
 
 const contactsOperations = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -35,7 +48,7 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   }
 };
 
-const arr = hideBin(process.argv);
-const { argv } = yargs(arr);
+// const arr = hideBin(process.argv);
+// const { argv } = yargs(arr);
 
 invokeAction(argv);
